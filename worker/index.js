cfc9b77c82c8959b4b1e564fd70a1d51af263c95
Vladimir Kolmakov <vkolmakov256@gmail.com>
2019-07-21 12:37:11 -0500

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) {
+    return 1;
+  }
+
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on("message", (channel, message) => {
+  // using message (index) as the key
+  console.log(`inserting a value for ${message}`);
+  redisClient.hset("values", message, fib(parseInt(message, 10)));
+});
+sub.subscribe("insert");
